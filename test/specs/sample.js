71ce8690d4a5f0ab22a5f7bab9b69075f444da4f
Sanjay Mali <malisanjay5433@gmail.com>
2023-01-04 12:34:14 +0530

    Sample test  playing around with the appium2.0 and webdriverIO

---
+++ test/specs/sample.js
@@ -0,0 +1,21 @@
+describe("CLARK-NATIVE APP AUTOMATION", () => {
+  it("ACCEPT COOKIES", async () => {
+    console.log("User is accepted cookies..");
+  });
+  it("REJECT COOKIES", async () => {
+    console.log("User is rejected cookies..");
+  });
+  it("MORE INFORMATION", async () => {
+    console.log("User would like to check more info..");
+  });
+});
+
+// get acceptCookies() {
+//     //     return $('//android.widget.Button[@text=" Alle Cookies akzeptieren"]');
+//     //   }
+//     //   get rejectCookies() {
+//     //     return $('//android.widget.Button[@text=" Ablehnen"]');
+//     //   }
+//     //   get moreInformation() {
+//     //     return $('//android.widget.Button[@text=" Mehr Informationen"]');
+//     //   }
