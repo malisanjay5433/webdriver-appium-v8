2ef5668decc76f246b0f6a086363c3be93f055f9
Sanjay Mali <malisanjay5433@gmail.com>
2023-01-05 18:08:30 +0530

    Accept cookies, Login and set values to the email and password textfields working

--- test/specs/sample.js
+++ test/specs/sample.js
@@ -1,10 +1,38 @@
 describe("CLARK-NATIVE APP AUTOMATION", () => {
   it("ACCEPT COOKIES", async () => {
     console.log("User is accepted cookies..");
+    // await driver.setImplicitWaitTimeout(100300);
+    // await this.driver.setImplicitTimeout(timeout);
+    // await this.pauseTest(100300);
+
+    await browser.setImplicitTimeout(100300);
+    const cookies = await $(
+      '//android.widget.Button[@text=" Alle Cookies akzeptieren"]'
+    );
+
+    await cookies.click();
+    await browser.pause(1000);
+
+    const login = await $('//*[@text="Einloggen"]');
+    await login.click();
+    await browser.pause(1000);
+
+    const inputLoginEmailTextField = await $(
+      '//*[@resource-id="mandate_login_email"]'
+    );
+    const inputLoginPasswordTextField = await $(
+      '//*[@resource-id="mandate_login_password"]'
+    );
+    await inputLoginEmailTextField.addValue("ssasasas");
+    await inputLoginPasswordTextField.addValue("Pass212");
+
+    await browser.pause(1000);
   });
+
   it("REJECT COOKIES", async () => {
     console.log("User is rejected cookies..");
   });
+
   it("MORE INFORMATION", async () => {
     console.log("User would like to check more info..");
   });
--- wdio.conf.js
+++ wdio.conf.js
@@ -55,25 +55,12 @@ export const config = {
   //
   capabilities: [
     {
-      // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-      // grid with only 5 firefox instances available you can make sure that not more than
-      // 5 instances get started at a time.
-      //   maxInstances: 5,
-      //   //
-      //   browserName: "chrome",
-      //   acceptInsecureCerts: true,
-      // If outputDir is provided WebdriverIO can capture driver session logs
-      // it is possible to configure which logTypes to include/exclude.
-      // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-      // excludeDriverLogs: ['bugreport', 'server'],
-
       "appium:platformName": "android",
       "appium:deviceName": "emulator-5554",
       "appium:automationName": "UiAutomator2",
-      "appium:app":
-        "/Users/sanjay/Documents/Automation/appium-webdriver01/webdriver-appium-v8/app/android/app-DE-debug.apk",
-      //   path.join(process.cwd(), "./app/android/app-DE-debug.apk"),
-      //    "app/android/app-DE-debug.apk",
+      "appium:app": "./app/android/app-DE-debug.apk",
+      "appium:autoGrantPermissions": true,
+      "appium:appWaitActivity": "*",
     },
   ],
   //
@@ -130,7 +117,7 @@ export const config = {
         args: {
           address: "localhost",
           port: 4723,
-          // relaxedSecurity: true,
+          relaxedSecurity: true,
         },
         logPath: "./",
       },
@@ -164,7 +151,8 @@ export const config = {
   // See the full list at http://mochajs.org/
   mochaOpts: {
     ui: "bdd",
-    timeout: 60000,
+    // timeout: 60000,
+    timeout: 1010000,
   },
   //
   // =====
